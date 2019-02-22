a4d6c8c2c89d31cf2d725a683c8a5f1dcbe81f2e
Piotr Szlaski <piotr.szlaski@ivanti.com>
2019-02-22 10:31:25 +0100

    form added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
-import { Examplecomponent } from './components/exampleComponent';
-import {List} from './components/List';
+import { List } from './components/List';
+import { Form } from './components/Form'
 class App extends Component {
   render() {
     return (
       <div className="row mt-5">
-      <div className="col-md-4 offset-md-1">
-      <h2>Articles</h2>
-        <List />
+        <div className="col-md-4 offset-md-1">
+          <h2>Articles</h2>
+          <List />
+        </div>
+        <div className="col-md-4 offset-md-1">
+          <h2>Add a new article</h2>
+          <Form />
+        </div>
       </div>
-    </div>
     );
   }
 }
