55cecf0c0ba2c77cb58ade54abffabca64f13129
Piotr Szlaski <piotr.szlaski@ivanti.com>
2019-02-22 09:07:28 +0100

    reducer v2

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,18 @@
+import { ADD_ARTICLE } from '../constants/actionTypes'
+
 const initialState = {
     articles: []
 }
 
-const rootReducer = (state = initialState, action) => state;
+const rootReducer = (state = initialState, action) => {
+    if (action.type === ADD_ARTICLE) {
+        return Object.assign(
+            {},
+            state,
+            { articles: state.articles.concat(action.payload) }
+            )
+    }
+    return state;
+};
 
 export default rootReducer;
