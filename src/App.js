337709cbaf61d7f390d245308aeecd3663f5b944
Piotr Szlaski <piotr.szlaski@ivanti.com>
2019-02-22 10:11:37 +0100

    costam

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { Examplecomponent } from './components/exampleComponent';
-
+import {List} from './components/List';
 class App extends Component {
   render() {
     return (
-      <div>
-        <Examplecomponent />
+      <div className="row mt-5">
+      <div className="col-md-4 offset-md-1">
+      <h2>Articles</h2>
+        <List />
       </div>
+    </div>
     );
   }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { ADD_ARTICLE } from '../constants/actionTypes'
 
 const initialState = {
-    articles: []
+    articles: [
+        { title: "React Redux Tutorial for Beginners", id: 1 },
+        { title: "Redux e React: cos'è Redux e come usarlo con React", id: 2 }
+    ]
 }
 
 const rootReducer = (state = initialState, action) => {
